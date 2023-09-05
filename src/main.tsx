7fca0a9628d977c30331f2000d4a892299e7bbf2
Dimitris Rakas <dimitrisrakas@gmail.com>
2023-09-05 15:05:02 +0300

    Initialize with vite

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+
+import { createRoot } from 'react-dom/client'
+
+import App from './App.tsx'
+
+const container = document.getElementById('root') as HTMLElement
+
+if (container) {
+    const root = createRoot(container)
+
+    root.render(
+        <StrictMode>
+            <App />
+        </StrictMode>
+    )
+}
